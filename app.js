11bb7a65fe46bec252fe844e1a2d98885c39c188
synti <synti.bogado@gmail.com>
2021-12-20 14:00:51 -0300

    agrego porcfile y modifi app

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen (3030, ()=> {
-    console.log('Exito total');
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res)=> {
